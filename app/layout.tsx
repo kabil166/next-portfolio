900cc35b6fc3d6d4a84051e73c3128a2169b71eb
Kabil Hussain <root1@Kabils-Air.lan>
2024-10-20 17:23:14 +0530

    resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
-import { MetadataRoute } from 'next'
+// import { MetadataRoute } from 'next'
 
 
 const geistSans = localFont({
